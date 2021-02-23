b02c6ddd0b2b2ef45c9be10cd87f5463be250c80
phsndev <78909025+phsndev@users.noreply.github.com>
2021-02-22 23:42:53 -0300

    Controller incompleto

--- src/main/java/br/com/ilia/digital/folhadeponto/controller/Controller.java
+++ src/main/java/br/com/ilia/digital/folhadeponto/controller/Controller.java
@@ -1,5 +1,16 @@
 package br.com.ilia.digital.folhadeponto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ilia.digital.folhadeponto.Service.RegistroService;
+
+@RestController
 public class Controller {
+	
+	@Autowired
+	private RegistroService registroService;
+	
+	@RequestMapping(value = "/v1/batidas", method = RequestMethod.GET)
 
 }
